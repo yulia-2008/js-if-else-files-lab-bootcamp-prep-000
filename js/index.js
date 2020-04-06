77b3283d03b3c3a7c68e4f005f072b9c5fa6b1e7
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-06 18:05:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
 
 // Don't worry about this
 module.exports = { name, height
-}
+};
 if(name==="Susan") {
-  console.log("The name is correct")
+  console.log("The name is correct");
 }
-'js/index.js'
+'js/index.js';
+
 
 
