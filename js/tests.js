1c3bbe4b38d66862bec519e8e500cf349f4b87d0
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-06 17:58:56 +0000

    Automatically backed up by Learn

--- js/tests.js
+++ js/tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 var name = "Susan";
-if (index.name === "Susan") {
+if (name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log("Expected: Susan, Received: "+ name)
 }
 
 
